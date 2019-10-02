9da88aee54801865b0106bcccd3423e19d08345a
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2019-10-02 16:43:01 +1000

    Update Atmosphere.py

--- Atmosphere.py
+++ Atmosphere.py
@@ -1,23 +1,23 @@
-# In this tutorial we will be looking at three university level programming questions and how to solve them.
-# some HighSchools will also feature some of these questions in their exams
 
-# Show a photo of the question
+#The Following code recieves input from the user and then classifies what atmosphere that altitude belongs to.
 
-# Altitude = int(input("Insert an altitude: "))
-# if 0 < Altitude <= 17:
-#     print("This Altitude belongs to the  Troposphere")
-# elif 17 < Altitude <= 60:
-#     print("This Altitude belongs to the Stratosphere")
-# elif 60 < Altitude <= 120:
-#     print("This Altitude belongs to the Mesosphere")
-# elif Altitude < 0:
-#     print("Out of Range")
-# else:
-#     print("This Altitude belongs to the Thermo and Exosphere")
+#!!!You have to comment one of the versions in order for this code to work.
+# Simple Version
+ Altitude = int(input("Insert an altitude: "))
+if 0 < Altitude <= 17:
+    print("This Altitude belongs to the  Troposphere")
+elif 17 < Altitude <= 60:
+    print("This Altitude belongs to the Stratosphere")
+elif 60 < Altitude <= 120:
+    print("This Altitude belongs to the Mesosphere")
+elif Altitude < 0:
+    print("Out of Range")
+else:
+    print("This Altitude belongs to the Thermo and Exosphere")
 
-# Start off showing them what it is in simple form then introduce the while loop.
+
 # I want the user to ask as many times as they can. So I will do the following
-
+#More Complicated Version
 while 3 > 2:
     Altitude = int(input("Insert an altitude: "))
     if 0 < Altitude <= 17:
