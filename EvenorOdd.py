a08164766346b3feaeec0872c110fc85de359182
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2019-10-03 10:48:21 +1000

    Update EvenorOdd.py

--- EvenorOdd.py
+++ EvenorOdd.py
@@ -1,3 +1,5 @@
+
+#Asks The User for Input then classifies the user input as Even or Odd.
 i = 0
 while i < 3:
     number = int(input("Enter a number to check if it's even or odd: "))
@@ -7,3 +9,4 @@ while i < 3:
     else:
         print(number, "Is an odd Number ")
     i += 1
+#EndOfFIle
