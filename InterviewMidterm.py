84858ae0c1fd10500f6cd54f9f741ada189dc1de
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2019-10-20 15:19:36 +1100

    Update and rename Midterm.py to InterviewMidterm.py

--- InterviewMidterm.py
+++ InterviewMidterm.py
@@ -1,10 +1,10 @@
 
-
+#Calculates the triple of the input.
 def triple():
     x = float(input("Insert number: "))
     print(x * 3)
 
-
+#Calculates the cube of the input.
 def Cube():
    x = float(input("Insert number: "))
    print(x * x * x)
