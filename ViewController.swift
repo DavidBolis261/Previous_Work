e254050fecb9d6fc6096424a4e1e969060a6380a
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2019-11-25 15:15:38 +1100

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  NFCAPPTEST3
+//
+//  Created by RnD Taggle on 23/10/19.
+//  Copyright © 2019 RnD Taggle. All rights reserved.
+//  This file recieves the NDF Data from a file called TestModel.swift. Which has a class called modely
+
+import UIKit
+import CoreNFC
+import Foundation
+
+
+
+
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    @IBOutlet weak var themess: UITextView!
+    
+    @IBAction func BtnG(_ sender: Any) {
+        performSegue(withIdentifier: "TOButtonPg", sender: self)
+    }
+    @IBAction func thirdpg(_ sender: Any) {
+        performSegue(withIdentifier: "ToAnoView", sender: self)
+    }
+    var nfcSession: NFCNDEFReaderSession?
+    var word = "None"
+    
+    @IBAction func ScanPressed(_ sender: Any) {
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invlidated: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        var result = ""
+        for payload in messages[0].records{
+            print("-> \(payload)")
+            result += String.init(data: payload.payload.advanced(by: 3), encoding: .ascii) ?? "Format not supported"
+        }
+        DispatchQueue.main.async {
+            self.themess.text = result
+            
+        }
+        
+        
+        
+    }
+    func getReady() -> String{
+        self.themess.text = "David"
+        return self.themess.text
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TOButtonPg"{
+            let vc = segue.destination as! sViewController
+            vc.omd = self.themess.text
+        } else if segue.identifier == "ToAnoView"{
+            let vc = segue.destination as! tViewController
+            vc.Textlong = self.themess.text
+        }
+        
+        
+        
+        
+    }
+    
+    
+}
+
