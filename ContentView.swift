aabf31b143ce458299d0af5dcd8fb8c2ddd300b2
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2020-04-27 18:55:19 +1000

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,60 +1,91 @@
 //
 //  ContentView.swift
-//  camera
+//  YoutubeTut
 //
-//  Created by David Bolis on 12/1/20.
+//  Created by David Bolis on 27/4/20.
 //  Copyright © 2020 David Bolis. All rights reserved.
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
-    @State var image: Image? = nil
-    @State var showingImagePicker = false
+    @State var UserName = ""
+    @State var UserAge = 25
+    @State var UserGender = 1
+    
     
     var body: some View {
-        VStack{
-            if image == nil{
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 150, height: 150)
-                    .overlay(Circle().stroke(Color.red, lineWidth: 5))
-                    .foregroundColor(Color.purple)
-            }else{
-                image?
-                    .resizable()
-                    .clipShape(Circle())
-                    .frame(width: 150, height: 150)
-                    .overlay(Circle().stroke(Color.red, lineWidth: 5))
-                    .foregroundColor(Color.purple)
+        ZStack{
+            Color.green
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack( alignment: .center, spacing: 40){
+                Text("Create Your Account!").font(.title).bold().foregroundColor(Color.orange)
+                    .shadow(color: .gray, radius: 1, x: 1.6, y: 1.6)
+                VStack(alignment: .center, spacing: 5){
+                    
+                    Text("Insert Your UserName Below!").foregroundColor(Color.orange).bold()
+                        .shadow(color: .gray, radius: 1, x: 1.6, y: 1.6)
+                    TextField("DavidBolis261", text: $UserName)
+                        .frame(width: UIScreen.main.bounds.width - 60, height: 50)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(color: .gray, radius: 1, x: 1.5, y: 1.5)
+                }
+                
+                VStack(){
+                    Text("Select Your Age!")
+                        .font(.body)
+                        .foregroundColor(.orange)
+                        .bold()
+                        .shadow(color: .gray, radius: 1, x: 1.6, y: 1.6)
+                        .padding()
+                    
+                    Stepper(value: $UserAge, in: 16...100)
+                    {
+                        Text("\(UserAge) years old!")
+                    }.foregroundColor(.white)
+                }
+                VStack{
+                    Text("Select Your Gender")
+                        
+                        .foregroundColor(.orange)
+                        .bold()
+                        .shadow(color: .gray, radius: 1, x: 1.6, y: 1.6)
+                        .offset(x: 0, y: 60)
+                    Picker(selection: $UserGender, label: Text("Gender")
+                        .foregroundColor(Color.blue)
+                    ){
+                        Text("Male").tag(1)
+                        Text("Female").tag(2)
+                        
+                    }.labelsHidden()
+                }
+                Button(action: {
+                    self.AddInfo(UserName: self.UserName, UserAge: self.UserAge, Gender: self.UserGender)
+                }) {
+                    Text("Register")
+                        .frame(width: 100, height: 30, alignment: .center).background(Color.orange).foregroundColor(Color.white).cornerRadius(10)
+                }
+                
             }
-            
-            
-            
-            Button("Put Your Face Here"){
-                self.showingImagePicker.toggle()
-            }
-            
-            
-        }.sheet(isPresented: $showingImagePicker, content: {
-            ImagePicker.shared.view
-    
-        }).onReceive(ImagePicker.shared.$image){
-            image in self.image = image
         }
-        
-        
-        
-        
+    }
+    func AddInfo(UserName: String, UserAge: Int, Gender: Int){
+        let db = Firestore.firestore()
+        db.collection("Customers").document().setData(["UserName": UserName, "UserAge": UserAge, "Gender": Gender])
         
     }
+    
+    
+    
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
