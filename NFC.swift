166209a8c91533664844bee2d5553b4d78ed5ead
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2020-06-26 15:48:23 +1000

    Update NFC.swift

--- NFC.swift
+++ NFC.swift
@@ -7,7 +7,7 @@
 //  This file recieves the NDF Data from a file called TestModel.swift. Which has a class called modely
 
 import UIKit
-import CoreNFC
+import CoreNFC // Import This In Order To Work With NFC.
 import Foundation
 
 
@@ -15,56 +15,38 @@ import Foundation
 
 class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
     
-    @IBOutlet weak var themess: UITextView!
+    @IBOutlet weak var themess: UITextView! //This is the TextView which Will Present the NFC Message on the Tag.
     
-   
-   
-    var nfcSession: NFCNDEFReaderSession?
+    var nfcSession: NFCNDEFReaderSession? // Allows us to call and start the NFC Reader Session allowing us to communicate with the Tag.
     
     
-    @IBAction func ScanPressed(_ sender: Any) {
+    @IBAction func ScanPressed(_ sender: Any) { // The Button which allows us to call the NFC Reader Session Declared on line 20.
         nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
-        nfcSession?.begin()
-        
-        
+        nfcSession?.begin() // Begins the session from line 24   
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) { //NFC Function
         print("The session was invlidated: \(error.localizedDescription)")
     }
     
-    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) { // NFC Function
         var result = ""
-        for payload in messages[0].records{
-            print("-> \(payload)")
+        for payload in messages[0].records{ 
+            print("-> \(payload)") //Prints the Entire NFC Payload on the Tag.
             result += String.init(data: payload.payload.advanced(by: 3), encoding: .ascii) ?? "Format not supported"
+            //Result is now assigned the NFC Message from the Payload.
         }
         DispatchQueue.main.async {
-            self.themess.text = result
+            self.themess.text = result // Pastes the Result in the Text View Declared in line 18.
             
-        }
-        
-        
-        
+        }   
+      }
     }
- 
-    
-   
-        
-        
-        
-        
-    }
-    
-    
 }
 
